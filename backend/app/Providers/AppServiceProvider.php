5c0e65fe9c1cf3d35f876ef088d6c1fcc59bbc44
horiciico <pophoria112005@gmail.com>
2026-06-07 19:14:04 +0300

    feat: configure Brevo mailer, Clerk authentication, and project dependencies

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Transport\Dsn;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        \Log::info('Registering brevo transport in MailManager');
+        Mail::extend('brevo', function (array $config = []) {
+            $factory = new BrevoTransportFactory();
+            return $factory->create(new Dsn(
+                'brevo+api',
+                'default',
+                config('services.brevo.key')
+            ));
+        });
     }
 }
