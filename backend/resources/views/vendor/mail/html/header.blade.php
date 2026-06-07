1aac6f5f7c48c2c3ebb5579eebebe70fbbd1a5d2
horiciico <pophoria112005@gmail.com>
2026-06-07 17:39:43 +0300

    feat: add custom email header with logo to mail vendor views

--- backend/resources/views/vendor/mail/html/header.blade.php
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ isset($message) ? $message->embed(public_path('images/edu-logo.jpeg')) : asset('images/edu-logo.jpeg') }}" class="logo" alt="Edu Logo" style="max-height: 50px; width: auto; max-width: 100%;">
+<img src="{{ rtrim(config('app.frontend_url') ?? 'http://localhost:5173', '/') }}/edu-logo.jpeg" class="logo" alt="Edu Logo" style="max-height: 50px; width: auto; max-width: 100%;">
 </a>
 </td>
 </tr>
